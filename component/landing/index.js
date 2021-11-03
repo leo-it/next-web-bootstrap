337ba34fe463dd7a08daf52e5fb3b2f950a981de
Leonardo Sainz <leo.database92@gmail.com>
2021-11-03 16:33:20 -0300

    correcciones estilos

--- component/landing/index.js
+++ component/landing/index.js
@@ -1,18 +1,16 @@
-import Header from '../header'
-import React from 'react'
-import Footer from '../footer'
-import AboutUs from '../aboutUs/AboutUs'
+import Header from "../header";
+import React from "react";
+import Footer from "../footer";
+import AboutUs from "../aboutUs/AboutUs";
 
 const Landing = () => {
-    return (
-        <div>
-            <Header/>
-            <AboutUs/>
+  return (
+    <div>
+      <Header />
+      <AboutUs />
+      <Footer />
+    </div>
+  );
+};
 
-            <Footer/>
-            
-        </div>
-    )
-}
-
-export default Landing
+export default Landing;
